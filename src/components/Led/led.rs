73b0cb37ab08d24c397c460efc0233b961dbe004
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2018-06-03 05:03:13 +0700

    ➕ add and apply `dimensioned` crate - make_units GRAPHICS to represent px and px/mm - use GRAPHICS unit system in `diameter` function - use SI unit system in `brightness` function

--- src/components/Led/led.rs
+++ src/components/Led/led.rs
@@ -1,9 +1,28 @@
+#[macro_use]
+extern crate dimensioned as dim;
+
+use dim::si::{Ampere, Unitless, Volt, Watt};
+
+make_units! {
+    GRAPHICS;
+    NUM: Number;
+    base {
+        PX: Pixel, "px";
+        MM: Millimeter, "mm", Length;
+    }
+    derived {
+        PX_PER_MM: PixelPerMilliMeter = (Pixel / Millimeter);
+    }
+    constants {}
+    fmt = true;
+}
+
 #[no_mangle]
-pub fn diameter(size: u8, scale: u8) -> u8 {
+pub fn diameter(size: Millimeter<u8>, scale: PixelPerMilliMeter<u8>) -> Pixel<u8> {
     size * scale
 }
 
 #[no_mangle]
-pub fn brightness(v_in: f32, i_in: f32, p_max: f32) -> f32 {
+pub fn brightness(v_in: Volt<f32>, i_in: Ampere<f32>, p_max: Watt<f32>) -> Unitless<f32> {
     v_in * i_in / p_max
 }
