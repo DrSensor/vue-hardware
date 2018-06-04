0236a8803b600a4ffba9e294c67909d60c3e501e
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2018-06-04 14:18:34 +0700

    🐛 correcting the equation - voltage drop affect the brightness

--- src/components/Led/led.rs
+++ src/components/Led/led.rs
@@ -2,6 +2,7 @@
 extern crate dimensioned as dim;
 
 use dim::si::{Ampere, Unitless, Volt, Watt};
+use dim::si::f32consts::{V};
 
 make_units! {
     GRAPHICS;
@@ -24,5 +25,11 @@ pub fn diameter(size: Millimeter<u8>, scale: PixelPerMilliMeter<u8>) -> Pixel<u8
 
 #[no_mangle]
 pub fn brightness(v_in: Volt<f32>, i_in: Ampere<f32>, p_max: Watt<f32>) -> Unitless<f32> {
-    v_in * i_in / p_max
+    let v_forward = 1.8 * V; // voltage drop (Vf)
+
+    if v_in >= v_forward {
+        (v_in - v_forward) * i_in / p_max
+    } else {
+        Unitless::new(0.)
+    }
 }
